8172014a734be03f2d21b96401d3c85bc6cdc358
woojae05 <dnwo05@naver.com>
2021-06-06 21:45:08 +0900

    fix: toggle-btn

--- main.js
+++ main.js
@@ -3,14 +3,17 @@
 
     const $toggles =document.querySelectorAll('.toggle');
     const $toggleBtn = document.getElementById('toggle-btn');
-
     $toggleBtn.addEventListener('click',function(){
         toggleElements();
     });
 
+    window.addEventListener('resize',function( ){
+        
+    });
     function toggleElements(){
         [].forEach.call($toggles, function (toggle) {
             toggle.classList.toggle('on');
         });
+
     }
-})(window.document);
+})(window,document);
