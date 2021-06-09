93cefcba1b4ce3fd2a6c6dc0789aa6cf7429581f
woojae05 <dnwo05@naver.com>
2021-06-09 19:58:35 +0900

    add: resizeEvent

--- main.js
+++ main.js
@@ -8,12 +8,20 @@
     });
 
     window.addEventListener('resize',function( ){
-        
+        if(window.innerWidth>1024){
+            offElements()
+        }
     });
     function toggleElements(){
         [].forEach.call($toggles, function (toggle) {
             toggle.classList.toggle('on');
         });
 
+    }
+
+    function offElements(){
+        [].forEach.call($toggles, function (toggle) {
+            toggle.classList.remove('on');
+        });
     }
 })(window,document);
